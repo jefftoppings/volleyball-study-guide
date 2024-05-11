787c94bfed73ec736f1fe0244815fe5f061f0c3d
Jeff Toppings <jeffrey.toppings@outlook.com>
2024-05-11 13:06:55 -0600

    feat: start displaying question and answers

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Observable } from 'rxjs';
+import { Question } from '../data/questions';
+import { QuestionService } from './question.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule, AsyncPipe],
   templateUrl: './question.component.html',
-  styleUrl: './question.component.scss'
+  styleUrl: './question.component.scss',
 })
 export class QuestionComponent {
-
+  currentQuestion$: Observable<Question> = inject(QuestionService).currentQuestion$;
 }
