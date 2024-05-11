590f62d2fa92850a4ac17cf8f41f62c07ad97873
Jeff Toppings <jeffrey.toppings@outlook.com>
2024-05-11 10:42:58 -0600

    feat: app/toolbar setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'volleyball-study-guide';
+  router: Router = inject(Router);
+
+  handleNavBack(): void {
+    // TODO
+    this.router.navigate(['/']);
+  }
+
+  handleNavForward(): void {
+    // TODO
+    this.router.navigate(['/']);
+  }
 }
